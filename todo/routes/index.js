a4fe2473b7010b6c667cd568e9e9e7bcdbdb15fb
aubin <aubinh92@gmail.com>
2019-04-19 12:45:43 -0400

    push to clean up and allow dynamic schedule

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var mongodb =require('mongodb');
-var username = "testman";
-var workhours = 480;
-var buffertime = 25;
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+localStorage.username = "testman";
+localStorage.workhours = 480;
+localStorage.buffertime = 0;
+localStorage.endtimelist=[16,1];
+localStorage.miscKill=0;
 
 function Comparator(a, b) {
     if (parseInt(a.priority) < parseInt(b.priority)) return -1;
@@ -17,13 +24,45 @@ function ComparatorD(a, b) {
     else return 0;
 }
 
+function timeCheck(){
+  var today = new Date();
+  //today.setTime(today.getTime()-today.getTimezoneOffset()*60*1000);
+  return today;
+}
+
+function timeCompare(end,begintime){
+  var current=timeCheck();
+  var current_hours=current.getHours();
+  var current_minutes=current.getMinutes();
+  //console.log(current_hours+" hours currently");
+  var current_string='01/01/2011 '+current_hours+':'+current_minutes+':00';
+  console.log(current_string);
+  var end_string='01/01/2011 '+end[0]+':'+end[1]+':00';
+  console.log(end_string);
+  var current_Fixed= Date.parse(current_string);
+  var end_fixed=Date.parse(end_string);
+  var diffMs= end_fixed-current_Fixed;
+  var diffDays = Math.floor(diffMs / 86400000); // days
+  var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+  var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+  console.log(diffDays + " days, " + diffHrs + " hours, " + diffMins + " minutes");
+  var timeRemaining=parseInt(diffHrs)*60+parseInt(diffMins);
+  console.log("Total Minutes left minus the Misc: " + timeRemaining);
+  if (timeRemaining<begintime){
+  return timeRemaining;
+}
+  else {
+    return begintime;
+  }
+}
+
 
 /* GET home page.*/
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Task Manager',
-    user:username,
-    time:workhours
+    user:localStorage.username,
+    time:localStorage.workhours
    });
 });
 
@@ -39,8 +78,18 @@ router.post('/verifyuser', function(req, res) {
     else {
       console.log("Connection Established!");
 
-      username = req.body.user;
-      workhours= req.body.workhours;
+      localStorage.username = req.body.user;
+      var endstr= req.body.endtime;
+      var startstr=req.body.starttime;
+      var miscmins=req.body.misctime;
+      localStorage.miscKill=miscmins;
+      var start=startstr.split(':');
+      var end=endstr.split(':');
+      localStorage.endtimelist=end;
+      var tmins=(parseInt(end[0])-parseInt(start[0]))*60+(parseInt(end[1])-parseInt(start[1]))-miscmins;
+      localStorage.workhours=tmins;
+      console.log("Brand new work mins: "+localStorage.workhours);
+
       res.redirect("thelist");
 
     }
@@ -53,7 +102,7 @@ router.post('/edittask', function(req, res) {
   var MongoClient = mongodb.MongoClient;
   var url = 'mongodb://localhost:27017/tracker';
   var item = mongodb.ObjectId(req.body.user);
-  console.log(item);
+  ///console.log(item);
 
   MongoClient.connect(url, function(err, db){
     if (err){
@@ -85,7 +134,7 @@ router.post('/edittask', function(req, res) {
 router.get('/thelist', function(req, res) {
   var MongoClient = mongodb.MongoClient;
   var url = 'mongodb://localhost:27017/tracker';
-
+  localStorage.workhours=timeCompare(localStorage.endtimelist, localStorage.workhours);
   MongoClient.connect(url, function(err, db){
     if (err){
       console.log("unable to connect to the server",err);
@@ -94,7 +143,7 @@ router.get('/thelist', function(req, res) {
       console.log("Connection Established!");
 
       var collection = db.collection('tasks');
-      collection.find({"user": username, "complete":false}).toArray(function(err, result){
+      collection.find({"user": localStorage.username, "complete":false}).toArray(function(err, result){
         if (err){
           res.send(err);
         } else if (result.length) {
@@ -126,7 +175,7 @@ router.get('/prioritize', function(req, res) {
       console.log("Connection Established!");
 
       var collection = db.collection('tasks');
-      collection.find({"user": username, "complete":false}).toArray(function(err, result){
+      collection.find({"user": localStorage.username, "complete":false}).toArray(function(err, result){
         if (err){
           res.send(err);
         } else if (result.length) {
@@ -139,12 +188,13 @@ router.get('/prioritize', function(req, res) {
           var totaltime=0;
           var totalh=0;
           var totalm=0;
-          var usabletime=workhours;
+          var usabletime=localStorage.workhours;
+          console.log("should be using this for priority: "+usabletime);
           for (var i = 0; i < newlist.length; i++){
             //console.log("This is the item im looking at: "+newlist[i]);
-            console.log("This is the time im looking at: "+newlist[i].time[0]+"hours "+newlist[i].time[1]+"minutes");
+            //console.log("This is the time im looking at: "+newlist[i].time[0]+"hours "+newlist[i].time[1]+"minutes");
             totaltime+=(parseInt(newlist[i].time[0])*60 + parseInt(newlist[i].time[1]))
-            if (totaltime<=workhours){
+            if (totaltime<=localStorage.workhours){
               allowed_list.push(newlist[i]);
               usabletime-=(parseInt(newlist[i].time[0])*60 + parseInt(newlist[i].time[1]));
             }
@@ -161,7 +211,7 @@ router.get('/prioritize', function(req, res) {
           }
           for (var i = 0; i < denied_list.length; i++){
             var totalt=(parseInt(denied_list[i].time[0])*60 + parseInt(denied_list[i].time[1]))
-            if (totalt<(buffertime+usabletime)){
+            if (totalt<(localStorage.buffertime+usabletime)){
               bonus_list.push(denied_list[i])
             }
 
@@ -197,7 +247,7 @@ router.get('/sortP', function(req, res) {
       console.log("Connection Established!");
 
       var collection = db.collection('tasks');
-      collection.find({"user": username, "complete": false}).toArray(function(err, result){
+      collection.find({"user": localStorage.username, "complete": false}).toArray(function(err, result){
         if (err){
           res.send(err);
         } else if (result.length) {
@@ -210,12 +260,12 @@ router.get('/sortP', function(req, res) {
           var totaltime=0;
           var totalh=0;
           var totalm=0;
-          var usabletime=workhours;
+          var usabletime=localStorage.workhours;
           for (var i = 0; i < newlist.length; i++){
             //console.log("This is the item im looking at: "+newlist[i]);
             console.log("This is the time im looking at: "+newlist[i].time[0]+"hours "+newlist[i].time[1]+"minutes");
             totaltime+=(parseInt(newlist[i].time[0])*60 + parseInt(newlist[i].time[1]))
-            if (totaltime<=workhours){
+            if (totaltime<=localStorage.workhours){
               allowed_list.push(newlist[i]);
               usabletime-=(parseInt(newlist[i].time[0])*60 + parseInt(newlist[i].time[1]));
             }
@@ -269,7 +319,7 @@ router.get('/sortD', function(req, res) {
       console.log("Connection Established!");
 
       var collection = db.collection('tasks');
-      collection.find({"user": username, "complete": false}).toArray(function(err, result){
+      collection.find({"user": localStorage.username, "complete": false}).toArray(function(err, result){
         if (err){
           res.send(err);
         } else if (result.length) {
@@ -304,7 +354,7 @@ router.get('/showcomplete', function(req, res) {
       console.log("Connection Established!");
 
       var collection = db.collection('tasks');
-      collection.find({"user": username, "complete": true}).toArray(function(err, result){
+      collection.find({"user": localStorage.username, "complete": true}).toArray(function(err, result){
         if (err){
           res.send(err);
         } else if (result.length) {
@@ -474,14 +524,14 @@ router.post('/changetask', function(req, res){
         } else {
           console.log('Connected to Server');
           console.log("The username is below");
-          console.log(username);
+          console.log(localStorage.username);
           // Get the documents collection
           var collection = db.collection('tasks');
 
           // Get the student data passed from the form
           var task1 = {name: req.body.task, time: [parseInt(req.body.hours),parseInt(req.body.minutes)],
             duedate: req.body.duedate, complete: false, priority: req.body.priority, subtask: false,
-            user: username};
+            user: localStorage.username};
 
           // Insert the student data into the database
           collection.insert([task1], function (err, result){
